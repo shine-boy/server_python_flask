30e8066663974194a6258d0871efdc281bac1e37
shine-boy <1948322788@qq.com>
2021-12-19 22:01:24 +0800

    test

--- bigData/pipelines.py
+++ bigData/pipelines.py
@@ -27,6 +27,7 @@ class BigdataPipeline(object):
         )
         db = connection[settings['MONGODB_DB']]
         self.collection = db[settings['MONGODB_COLLECTION']]
+        print(self.collection)
 
     # def process_item(self, item, spider):
     #     # try:
@@ -59,7 +60,7 @@ class BigdataPipeline(object):
         #
         #     data=data.append(temp,ignore_index=True)
         # data.to_csv(path,index=False)
-        print(item)
+        # print(item)
         item['time']=datetime.datetime.now()
         self.collection.delete_many({'id':item["id"]})
         self.collection.insert(dict(item))
